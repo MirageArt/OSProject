3f9571d9d52228193b7c3afd481dc92a5b704dcc
Mira Hamdan <mira.m.hamdan@gmail.com>
2025-04-07 22:57:08 +0400

    Update consumer.c

--- consumer.c
+++ consumer.c
@@ -7,17 +7,23 @@ extern Buffer buffer;
 
 void* consumer(void* arg) {
     int id = *(int*)arg;
+    int consumed_count = 0;
+
     while (1) {
         sem_wait(&buffer.full);
         pthread_mutex_lock(&buffer.mutex);
 
-        int item = remove_item(&buffer);
-        printf("[Consumer-%d] Consumed: %d\n", id, item);
+        BufferItem item = remove_item(&buffer);
+        if (item.priority != -1) { // Only process valid items
+            log_message("Consumer", id, "Consumed item", item.value, item.priority);
+            printf("Buffer state: in=%d, out=%d\n", buffer.in, buffer.out);
+            consumed_count++;
+        }
 
         pthread_mutex_unlock(&buffer.mutex);
         sem_post(&buffer.empty);
 
-        usleep(150000);  // 150ms delay
+        usleep(150000); // 150ms delay
     }
     return NULL;
 }
