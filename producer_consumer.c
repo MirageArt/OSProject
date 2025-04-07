eddaca1e8fcb0feb653df3a2941d3f93b35b2c76
Mira Hamdan <mira.m.hamdan@gmail.com>
2025-04-07 22:59:53 +0400

    Update producer_consumer.c

--- producer_consumer.c
+++ producer_consumer.c
@@ -5,14 +5,28 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <unistd.h>
+#include <signal.h>
+#include <time.h>
 
-Buffer buffer;  // Global buffer instance
+Buffer buffer;
+volatile sig_atomic_t stop = 0;
+
+void handle_signal(int sig) {
+    stop = 1;
+}
 
 int main(int argc, char* argv[]) {
     if (argc != 4) {
         printf("Usage: %s <num_producers> <num_consumers> <buffer_size>\n", argv[0]);
         return 1;
     }
+
+    signal(SIGINT, handle_signal);
+
+    // Seed random number generator once using high-precision clock
+    struct timespec ts;
+    clock_gettime(CLOCK_MONOTONIC, &ts);
+    srand((unsigned)ts.tv_nsec);
 
     int num_producers = atoi(argv[1]);
     int num_consumers = atoi(argv[2]);
@@ -42,12 +56,22 @@ int main(int argc, char* argv[]) {
         pthread_join(producers[i], NULL);
     }
 
+    // Wait for consumers to finish remaining items
+    while (!stop) {
+        int val;
+        sem_getvalue(&buffer.full, &val);
+        if (val == 0) break;
+        usleep(100000);
+    }
+
     // Terminate consumers
-    sleep(1);
     for (int i = 0; i < num_consumers; i++) {
         pthread_cancel(consumers[i]);
+        pthread_join(consumers[i], NULL);
+        log_message("System", 0, "Terminated consumer", i + 1, -1);
     }
 
     destroy_buffer(&buffer);
+    log_message("System", 0, "All threads terminated", 0, -1);
     return 0;
 }
